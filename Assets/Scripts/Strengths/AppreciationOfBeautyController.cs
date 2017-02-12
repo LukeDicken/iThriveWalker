bd0cca7c518a7a42538ed4762fe64272f4d1a29e
Brie Code <truluv@truluvmedia.com>
2017-02-11 21:03:45 -0800

    Day/Night cycle speeds up when stopped and looking around.

---
+++ Assets/Scripts/Strengths/AppreciationOfBeautyController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class AppreciationOfBeautyController : MonoBehaviour 
+{
+	public float minutesInDayMax = 1f;
+	public float minutesInDayMin = 0.1f;
+	public float timeToDayMin = 10f;
+
+	public GameObject dayNight;
+
+	float timeSinceMovement = 0f;
+
+	void Start() 
+	{
+		timeSinceMovement = 0f;
+	}
+	
+	void Update()
+	{
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+		FirstPersonController firstPersonController = player.GetComponent<FirstPersonController>();
+
+		if (firstPersonController.Moving)
+		{
+			timeSinceMovement = 0f;
+			dayNight.GetComponent<DayNightStart>().minutesInDay = minutesInDayMax;
+		} else
+		{
+			timeSinceMovement += Time.deltaTime;
+		
+			if (timeSinceMovement > timeToDayMin)
+				timeSinceMovement = timeToDayMin;
+
+			float day = (minutesInDayMin - minutesInDayMax)*timeSinceMovement/timeToDayMin + minutesInDayMax;
+			dayNight.GetComponent<DayNightStart>().minutesInDay = day;
+		}
+	}
+}
