03f6e749fcdde160bb4fad41877dc4dd30321475
LukeDicken <lukedicken@gmail.com>
2017-02-11 21:54:52 -0800

    Curiosity Spawner

--- Assets/Scripts/CuriositySpawner.cs
+++ Assets/Scripts/CuriositySpawner.cs
@@ -7,10 +7,23 @@ using UnityEngine;
 public class CuriositySpawner : MonoBehaviour {
 
 	public GameObject[] points;
+	public int spawnRadius;
+	public int minDistance;
+	private GameObject player;
+	private List<GameObject> currentPOIs;
+	private List<GameObject> ignoreList;
+	private WorldManager wm;
 
 	// Use this for initialization
 	void Start () {
+		// grab the player GO so we can track where it goes
+
+		currentPOIs = new List<GameObject> ();
+		ignoreList = new List<GameObject> ();
+		player = GameObject.FindGameObjectWithTag("Player");
+		wm = GameObject.FindGameObjectWithTag ("WorldManager").GetComponent<WorldManager> ();
 		// spawn a POI
+		spawn();
 	}
 	
 	// Update is called once per frame
@@ -19,5 +32,41 @@ public class CuriositySpawner : MonoBehaviour {
 		// log to analytics
 		// spawn another
 			// nearby?
+		int spawnCount = 0;
+		foreach (GameObject go in currentPOIs) {
+			float dist = Vector3.Magnitude (player.transform.position - go.transform.position);
+			if (dist < 5.0f && !(ignoreList.Contains(go))) {
+				// reached
+				//deletion.Add(go);
+				ignoreList.Add(go);
+				spawnCount++;
+			}
+		}
+		while(spawnCount > 0)
+		{
+			spawn ();
+			spawnCount--;
+		}
+	}
+
+	public void spawn()
+	{
+		bool placeable = false;
+		while(!placeable)
+		{
+			// pick a location on the unit circle * radius
+			Vector2 r = Random.insideUnitCircle * spawnRadius;
+			Vector3 pos = new Vector3(r.x, 0, r.y) + player.transform.position;
+			// ask WorldManager if that type is placeable
+			// if yes, spawn a random object
+			// ensure a minimum distance
+			if (Vector3.Magnitude (player.transform.position - pos) >= minDistance && wm.isValidForPlacement(wm.getTypeFromPosition(new Vector2(pos.x, pos.z)))) {
+				int rand = Random.Range (0, this.points.Length);
+				GameObject newGO = GameObject.Instantiate (points [rand], pos, player.transform.rotation) as GameObject;
+				currentPOIs.Add (newGO);
+				Debug.Log (newGO.name);
+				placeable = true;
+			}
+		}
 	}
 }
--- Assets/Scripts/Generators/WorldManager.cs
+++ Assets/Scripts/Generators/WorldManager.cs
@@ -11,11 +11,8 @@ public class WorldManager : MonoBehaviour {
 	public GameObject[] gos;
 	public int[,] map;
 	// Use this for initialization
-	IEnumerator Start () {
-		Debug.Log ("Top of the method");
-		yield return StartCoroutine(loadFromFile());
-		//loadFromFile ();
-		Debug.Log("Coroutine running");
+	void Start () {
+		loadFromFile();
 	}
 	
 	// Update is called once per frame
@@ -23,18 +20,30 @@ public class WorldManager : MonoBehaviour {
 
 	}
 
-	IEnumerator loadFromFile()
+	public bool isValidForPlacement(int type)
+	{
+		return !(type == 0); // only water is not valid for placement
+	}
+
+	public int getTypeFromPosition(Vector2 position)
+	{
+		Vector2 notoffset = position + new Vector2 (15, 15);
+		int wholeX = (int)Math.Floor (notoffset.x / 15.0f);
+		int wholeY = (int)Math.Floor (notoffset.y / 15.0f);
+		return map [wholeX, wholeY];
+	}
+
+	void loadFromFile()
 	{
 		// load in from file
-		yield return new WaitForEndOfFrame();
 		string text = System.IO.File.ReadAllText(fileName);
 		// parse from JSON
 		var dict = Json.Deserialize(text) as Dictionary <string, System.Object>;
 			// build tileSet
 		List<System.Object> layers = dict["layers"] as List<System.Object>;
 		Dictionary <string, System.Object> layerZero = layers [0] as Dictionary<string, System.Object>;
-		long width = (long)layerZero["width"];
-		long height = (long)layerZero ["height"];
+		int width = (int)((long)layerZero["width"]);
+		int height = (int)((long)layerZero ["height"]);
 		List<System.Object> data = layerZero ["data"] as List<System.Object>;
 		List<System.Object> tilesets = dict ["tilesets"] as List<System.Object>;
 		Dictionary<string, System.Object> setZero = tilesets [0] as Dictionary<string, System.Object>;
@@ -44,6 +53,7 @@ public class WorldManager : MonoBehaviour {
 			// Debug.Log("Width " + width);
 			// Debug.Log("Height " + height);
 		int counter = 0;
+		map = new int[height, width];
 		for (int i = (int) height-1; i >= 0; i--) {
 			for (int j = 0; j < width; j++) {
 				// need the lookup
@@ -56,7 +66,9 @@ public class WorldManager : MonoBehaviour {
 				//Debug.Log (GOindex);
 				GameObject go = this.gos[GOindex];
 				//Debug.Log (go.name);
-				GameObject.Instantiate (go, this.gameObject.transform.position+(j*Vector3.right*15)+(i*Vector3.forward*15), this.gameObject.transform.rotation);
+				GameObject newGO = GameObject.Instantiate (go, this.gameObject.transform.position+(j*Vector3.right*15)+(i*Vector3.forward*15), this.gameObject.transform.rotation) as GameObject;
+				newGO.transform.parent = this.gameObject.transform;
+				map [j, i] = GOindex;
 				// instantiate
 				counter++;
 				//yield return new WaitForEndOfFrame ();
@@ -67,7 +79,6 @@ public class WorldManager : MonoBehaviour {
 		IDictionary<string, System.Object> eventData = new Dictionary<string, object> ();
 		eventData.Add ("mapHash", dataHash);
 		Analytics.CustomEvent ("Tiled map loaded", eventData); 
-		yield return new WaitForEndOfFrame();
 
 	}
 		
