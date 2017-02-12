65f638e93e01fdf75ec8b86e58533b6a616c643a
LukeDicken <lukedicken@gmail.com>
2017-02-11 18:27:38 -0800

    New prefabs, material changes, tile loader works, main scene

--- Assets/Scripts/Generators/TiledLoader.cs
+++ Assets/Scripts/Generators/TiledLoader.cs
@@ -1,16 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using MiniJSON;
 
 public class TiledLoader : MonoBehaviour {
 
+	public string fileName;
+	public GameObject[] gos;
 	// Use this for initialization
 	void Start () {
-		
+		loadFromFile (fileName);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+	void loadFromFile(string filename)
+	{
+		// load in from file
+		string text = System.IO.File.ReadAllText(filename);
+		// parse from JSON
+		var dict = Json.Deserialize(text) as Dictionary <string, System.Object>;
+			// build tileSet
+		List<System.Object> layers = dict["layers"] as List<System.Object>;
+		Dictionary <string, System.Object> layerZero = layers [0] as Dictionary<string, System.Object>;
+		long width = (long)layerZero["width"];
+		long height = (long)layerZero ["height"];
+		List<System.Object> data = layerZero ["data"] as List<System.Object>;
+		List<System.Object> tilesets = dict ["tilesets"] as List<System.Object>;
+		Dictionary<string, System.Object> setZero = tilesets [0] as Dictionary<string, System.Object>;
+		Dictionary<string, System.Object> tiles = setZero ["tileproperties"] as Dictionary<string, System.Object>;
+
+			// step across the data array and instantiate
+			// Debug.Log("Width " + width);
+			// Debug.Log("Height " + height);
+		int counter = 0;
+		for (int i = (int) height-1; i >= 0; i--) {
+			for (int j = 0; j < width; j++) {
+				// need the lookup
+				long tile = (long)data[counter];
+				string index = tile.ToString ();
+				Dictionary<string, System.Object> thisTile = tiles[index] as Dictionary<string, System.Object>;
+				string etid = thisTile ["ETID"] as String;
+				int GOindex = Int32.Parse (etid);
+				//Debug.Log (GOindex);
+
+				GameObject.Instantiate (this.gos [GOindex], this.gameObject.transform.position+(j*Vector3.right*15)+(i*Vector3.forward*15), this.gameObject.transform.rotation);
+				// instantiate
+				counter++;
+			}
+
+		}
+		//
+	}
+		
 }
