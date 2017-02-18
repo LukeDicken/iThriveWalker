08c78a3780dbcb7098865a6162283ed5cf5a313c
LukeDicken <luke@cerebrus-net.com>
2017-02-18 13:28:56 -0800

    Created logging and analytics utility classes

---
+++ Assets/Scripts/Utility/AnalyticsWrapper.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * 
+ * AnalyticsWrapper.cs -- A wrapper for Analytics calls
+ * Created 2/18/17
+ * 
+ * Build and send analytics calls from here. Wrapped so that calls can be
+ * standardised with playerIDs and whatnot here, rather than having to do
+ * so on a per call basis. Also means that using UnityAnalytics becomes
+ * swappable based on changing this single class rather than having a major
+ * edit job.
+ * 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class AnalyticsWrapper {
+
+	
+}
---
+++ Assets/Scripts/Utility/LogWrapper.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * 
+ * Logger.cs -- A Debug.Log() / Debug.Error() wrapper.
+ * Created 2/18/17
+ * 
+ * Why a wrapper? Because then from one place we can change the log/error
+ * behaviour, enforce warn levels etc. Also means that longer term things might
+ * get redirected so instead of using the Unity Debug system, we could output
+ * to a text file or to something like splunk - by maintaining this one class
+ * 
+ */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class LogWrapper { // does not need to inherit from MonoBehaviour
+
+    private static bool suppressErrors = false;
+
+	public static void Log(string message)
+    {
+        Debug.Log(message);
+    }
+
+    public static void Error(string message)
+    {
+        if(suppressErrors)
+        {
+            Log("Suppressed Error: " + message);
+        }
+    }
+}
